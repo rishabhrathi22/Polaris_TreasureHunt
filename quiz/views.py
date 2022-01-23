97b69c98ae065194ed555a9d45f2e3491d463927
Rishabh <rishabhrathi000@gmail.com>
2022-01-23 12:54:49 +0530

    riddle integrated

--- quiz/views.py
+++ quiz/views.py
@@ -7,218 +7,241 @@ from django.contrib.auth.models import User
 
 from django.utils import timezone
 
+
 def landing(request):
-	return render(request, 'home.html')
+    current_user = request.user
+    print(current_user)
+    if current_user.is_anonymous:
+        return redirect('/accounts/login')
+    return render(request, 'home.html')
 
 
 def home(request):
-	current_user = request.user
+    current_user = request.user
 
-	# user not logged in
-	if current_user.is_anonymous:
-		return redirect('/accounts/login')
+    # user not logged in
+    if current_user.is_anonymous:
+        return redirect('/accounts/login')
 
-	return render(request, "index.html")
+    return render(request, "index.html")
 
-	# try:
-	# 	user_data = UserData.objects.get(user = current_user)
-	# except Exception as e:
-	# 	print(e)
-	# 	user_data = UserData.objects.create(user = current_user)
-	# 	user_data.save()
+    # try:
+    # 	user_data = UserData.objects.get(user = current_user)
+    # except Exception as e:
+    # 	print(e)
+    # 	user_data = UserData.objects.create(user = current_user)
+    # 	user_data.save()
 
-	# context = {
-	# 	"score": user_data.score,
-	# 	"hints_taken": user_data.hints_taken
-	# }
+    # context = {
+    # 	"score": user_data.score,
+    # 	"hints_taken": user_data.hints_taken
+    # }
 
-	# return render(request, 'index.html', context = context)
+    # return render(request, 'index.html', context = context)
 
 
 def solve(request):
-	current_user = request.user
+    current_user = request.user
 
-	# user not logged in
-	if current_user.is_anonymous:
-		return redirect('/accounts/login')
+    # user not logged in
+    if current_user.is_anonymous:
+        return redirect('/accounts/login')
 
-	# if this is a POST request we need to process the form data
-	if request.method == 'POST':
-		# create a form instance and populate it with data from the request:
-		form = SubmitRiddleForm(request.POST)
-		# check whether it's valid:
-		if form.is_valid():
-			# process the data in form.cleaned_data as required
-			data = form.cleaned_data
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SubmitRiddleForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            data = form.cleaned_data
 
-			# check answer
-			try:
-				riddle = Riddle.objects.get(ques_no = data['ques_no'])
-				user_ans = data['answer']
-				correct_ans = riddle.answer
+            # check answer
+            try:
+                riddle = Riddle.objects.get(ques_no=data['ques_no'])
+                user_ans = data['answer'].lower()
+                correct_ans = riddle.answer.lower()
 
-				if (user_ans == correct_ans):
-					# update the end time for ques
-					hist = History.objects.get(user = current_user, ques = riddle)
-					hist.end_time = timezone.now()
-					hist.save()
+                user_data = UserData.objects.get(user=current_user)
 
-					# calculate score according to time taken
-					time_taken = hist.end_time - hist.start_time
-					time_taken_mins = time_taken.total_seconds()//60
+                if(user_data.ques_solved + 1 != data['ques_no']):
+                    return redirect('/solve')
 
-					# score algo
-					max_points = riddle.correct_points
-					curr_score = max(max_points//2, max_points - time_taken_mins*10)
+                if (user_ans == correct_ans):
+                    # update the end time for ques
+                    hist = History.objects.get(user=current_user, ques=riddle)
+                    hist.end_time = timezone.now()
+                    hist.save()
 
-					user_data = UserData.objects.get(user = current_user)
-					user_data.score += curr_score
-					user_data.ques_solved += 1
-					user_data.save()
+                    # calculate score according to time taken
+                    time_taken = hist.end_time - hist.start_time
+                    time_taken_mins = time_taken.total_seconds()//60
 
-					return render(request, 'correct.html')
-				else:
-					return render(request, 'incorrect.html')
+                    # score algo
+                    max_points = riddle.correct_points
+                    curr_score = max(
+                        max_points//2, max_points - time_taken_mins*10)
 
-			except Exception as e:
-				print(e)
-				return HttpResponse("Riddle not found")
+                    user_data.score += curr_score
+                    user_data.ques_solved += 1
+                    user_data.save()
 
-			# redirect to a new URL:
-			return redirect('/solve')
+                    return render(request, 'correct.html')
+                else:
+                    return render(request, 'incorrect.html')
 
-		else:
-			return HttpResponse("Incorrect form data")
+            except Exception as e:
+                print(e)
+                return HttpResponse("Riddle not found")
 
-	# if a GET (or any other method) we'll create a blank form
-	else:
-		# find the last solved question
-		try:
-			user_data = UserData.objects.get(user = current_user)
-			last_ques = user_data.ques_solved
-			# history = History.objects.filter(user = current_user, end_time__isnull = False)
-			# last_ques = history.last().ques.ques_no
-		except Exception as e:
-			print(e)
-			last_ques = 0
+            # redirect to a new URL:
+            # return redirect('/solve')
 
-		# get the next ques
-		curr_ques = last_ques + 1
-		try:
-			curr_riddle = Riddle.objects.get(ques_no = curr_ques)
-		except Exception as e:
-			print(e)
-			return HttpResponse("All riddles solved")
+        else:
+            return HttpResponse("Incorrect form data")
 
-		# start timer for current riddle
-		try:
-			hist = History.objects.create(user = current_user, ques = curr_riddle)
-			hist.save()
-		except Exception as e:
-			print(e)
-			hist = History.objects.get(user = current_user, ques = curr_riddle)
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        # find the last solved question
+        try:
+            user_data = UserData.objects.get(user=current_user)
+            last_ques = user_data.ques_solved
+            # history = History.objects.filter(user = current_user, end_time__isnull = False)
+            # last_ques = history.last().ques.ques_no
+        except Exception as e:
+            print(e)
+            last_ques = 0
 
-		context = {
-			'ques_no': curr_riddle.ques_no,
-			'hint_points': curr_riddle.hint_points,
-			'team_name': user_data.user,
-			'score': user_data.score,
-			'hints_taken': user_data.hints_taken,
-			'start_time': hist.start_time.timestamp()*1000
-		}
+        # get the next ques
+        curr_ques = last_ques + 1
+        try:
+            curr_riddle = Riddle.objects.get(ques_no=curr_ques)
+        except Exception as e:
+            print(e)
+            print("All riddles solved")
+            return redirect('/leaderboard')
 
-		return render(request, 'riddles/riddle' + str(curr_ques) + '.html', context)
+        # start timer for current riddle
+        try:
+            hist = History.objects.create(user=current_user, ques=curr_riddle)
+            hist.save()
+        except Exception as e:
+            print(e)
+            hist = History.objects.get(user=current_user, ques=curr_riddle)
+
+        # check is user has already taken hint
+        hintTaken = False
+        try:
+            hint_data = HintData.objects.get(
+                user=current_user, ques=curr_riddle)
+            if(hint_data):
+                hintTaken = True
+        except Exception as e:
+            print(e)
+
+        context = {
+            'ques_no': curr_riddle.ques_no,
+            'hint_points': curr_riddle.hint_points,
+            'team_name': user_data.user,
+            'score': user_data.score,
+            'hints_taken': user_data.hints_taken,
+            'start_time': hist.start_time.timestamp()*1000,
+            'isHintTaken': hintTaken
+        }
+
+        return render(request, 'riddles/riddle' + str(curr_ques) + '.html', context)
 
 
 def leaderboard(request):
-	current_user = request.user
+    current_user = request.user
 
-	# user not logged in
-	if current_user.is_anonymous:
-		return redirect('/accounts/login')
+    # user not logged in
+    if current_user.is_anonymous:
+        return redirect('/accounts/login')
 
-	try:
-		# all_users = UserData.objects.all()
-		all_users = UserData.objects.order_by('-ques_solved', '-score', 'hints_taken')
+    try:
+        # all_users = UserData.objects.all()
+        all_users = UserData.objects.order_by(
+            '-ques_solved', '-score', 'hints_taken')
 
-		data = []
-		curr_user_data = {}
-		rank = 1
+        data = []
+        curr_user_data = {}
+        rank = 1
 
-		for user in all_users:
-			data.append({
-				"rank": rank,
-				"user": user.user.username,
-				"ques_solved": user.ques_solved,
-				"score": user.score,
-				"hints_taken": user.hints_taken,
-			})
+        for user in all_users:
+            data.append({
+                "rank": rank,
+                "user": user.user.username,
+                "ques_solved": user.ques_solved,
+                "score": user.score,
+                "hints_taken": user.hints_taken,
+            })
 
-			if(user.user == current_user):
-				curr_user_data = {
-					"rank": rank,
-					"user": user.user.username,
-					"ques_solved": user.ques_solved,
-					"score": user.score,
-					"hints_taken": user.hints_taken,
-				}
+            if(user.user == current_user):
+                curr_user_data = {
+                    "rank": rank,
+                    "user": user.user.username,
+                    "ques_solved": user.ques_solved,
+                    "score": user.score,
+                    "hints_taken": user.hints_taken,
+                }
 
-			rank += 1
+            rank += 1
 
-	except Exception as e:
-		print(e)
-		return HttpResponse("User not found")
+    except Exception as e:
+        print(e)
+        return HttpResponse("User not found")
 
-	context = {
-		"all_users": data,
-		"curr_user": curr_user_data
-	}
+    context = {
+        "all_users": data,
+        "curr_user": curr_user_data
+    }
 
-	return render(request, 'leaderboard.html', context = context)
+    return render(request, 'leaderboard.html', context=context)
 
 
 def get_hint(request):
-	current_user = request.user
+    current_user = request.user
 
-	# user not logged in
-	if current_user.is_anonymous:
-		return redirect('/accounts/login')
+    # user not logged in
+    if current_user.is_anonymous:
+        return redirect('/accounts/login')
 
-	# check if ques number provided
-	if request.GET.get("ques", None) == None:
-		return JsonResponse({"error" : "Give question number"}, safe = False)
+    # check if ques number provided
+    if request.GET.get("ques", None) == None:
+        return JsonResponse({"error": "Give question number"}, safe=False)
 
-	# check if invalid question number
-	ques_no = int(request.GET["ques"])
-	if ques_no < 1 or ques_no > 8:
-		return JsonResponse({"error" : "Invalid question number"}, safe = False)
+    # check if invalid question number
+    ques_no = int(request.GET["ques"])
+    if ques_no < 1 or ques_no > 8:
+        return JsonResponse({"error": "Invalid question number"}, safe=False)
 
-	try:
-		riddle = Riddle.objects.get(ques_no = ques_no)
-		hint = riddle.hint
+    try:
+        riddle = Riddle.objects.get(ques_no=ques_no)
+        hint = riddle.hint
 
-		# check whether this ques hint already taken by user
-		try:
-			hint_data = HintData.objects.get(user = current_user, ques = riddle)
-			return JsonResponse({"hint" : hint}, safe = False)
-		except Exception as e:
-			print(e)
+        # check whether this ques hint already taken by user
+        try:
+            hint_data = HintData.objects.get(user=current_user, ques=riddle)
+            return JsonResponse({"hint": hint}, safe=False)
+        except Exception as e:
+            print(e)
 
-		user = UserData.objects.get(user = current_user)
+        user = UserData.objects.get(user=current_user)
 
-		# check whether user has enough points
-		if (user.score < riddle.hint_points):
-			return JsonResponse({"error" : "You don't have enough points"}, safe = False)
+        # check whether user has enough points
+        if (user.score < riddle.hint_points):
+            return JsonResponse({"error": "You don't have enough points"}, safe=False)
 
-		user.score -= riddle.hint_points
-		user.hints_taken += 1
-		user.save()
+        user.score -= riddle.hint_points
+        user.hints_taken += 1
+        user.save()
 
-		hint_data = HintData.objects.create(user = current_user, ques = riddle)
-		hint_data.save()
+        hint_data = HintData.objects.create(user=current_user, ques=riddle)
+        hint_data.save()
 
-		return JsonResponse({"hint" : hint}, safe = False)
-	except Exception as e:
-		print(e)
+        return JsonResponse({"hint": hint, "points": user.score}, safe=False)
+    except Exception as e:
+        print(e)
 
-	return JsonResponse({"error" : "Check Logs"}, safe = False)
+    return JsonResponse({"error": "Check Logs"}, safe=False)
